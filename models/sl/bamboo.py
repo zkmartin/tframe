43b022c525d7dda3b1be21a48624bcd4e288ede0
zkmartin <653938079@qq.com>
2018-04-14 16:36:07 +0800

    ...

--- models/sl/bamboo.py
+++ models/sl/bamboo.py
@@ -220,7 +220,7 @@ class Bamboo(Predictor):
     FLAGS.overwrite = False
     self.launch_model(FLAGS.overwrite and FLAGS.train)
     self.set_branch_index(self.branches_num + 1)
-    self._optimizer_lr_modify(lr_list[self.branches_num]*0.01)
+    self._optimizer_lr_modify(lr_list[-1])
     self._train_step = self._optimizer.minimize(loss=self._loss)
     Predictor.train(self, *args, **kwargs)
 
